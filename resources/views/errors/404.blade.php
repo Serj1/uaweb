1d1abee86d59461c3492725ebef0eafbffe9e0ae
serj1 <serj.chlek@gmail.com>
2015-05-23 16:27:40 +0300

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,150 @@
-404 Not found
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <!-- IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <![endif]-->
+
+    <script src='js/jquery-1.9.1.min.js'></script>
+    <script src='js/owl.carousel.min.js'></script>
+</head>
+<body class="body-page">
+<header class="header">
+    <div class="header-main">
+        <div class="header-left">
+            <div class="header-left__logo">
+                <a href="index.html"><img src="images/logo.png" height="29" width="65" alt=""></a>
+            </div>
+            <div class="product_day_and_nav">
+            </div>
+        </div>
+        <div class="header-right">
+            <div class="header-right__top">
+                <div class="title">
+                    <p class="title-text title-text_blue ">Ми зібрали разом кращі українські товари та бренди</p>
+
+                    <p class="title-text title-text_yellow">Вам залишилося обрати те, що дійсно до вподоби</p>
+                </div>
+                <div class="select-city">
+                    <ul class="menu-city">
+                        <li class="item-city"><a href="#">Кам'янець-Подільський</a></li>
+                        <li class="hidden-city"><a href="#">Київ</a></li>
+                        <li class="hidden-city"><a href="#">Львів</a></li>
+                    </ul>
+                </div>
+                <div class="lang">
+                    <ul class="menu-lang">
+                        <li class="item-lang">
+                            <a href="#">
+                                <img src="images/ukraine.png" height="20" width="29" alt="">
+                            </a>
+                        </li>
+                        <li class="hidden-lang">
+                            <a href="#">
+                                <img src="images/ukraine.png" height="20" width="29" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="header-right__bottom">
+                <div class="header-search">
+                    <form>
+                        <div class="search">
+                            <input type="text" name="search" placeholder="Введіть назву товару або бренду">
+                            <input type="submit" class="loupe" value="">
+                        </div>
+                    </form>
+                    <div class="count-searching">
+                        <p class="show-statistic"><span>1 764 зараз шукають</span></p>
+                    </div>
+                </div>
+                <div class="for-user">
+                    <ul>
+                        <li class="sing-in"><a href="#">Вхід</a></li>
+                        <li class="favorite"><a href="#">Вибране</a></li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</header>
+<section class="main-block">
+    <div class="main-block__left">
+
+    </div>
+    <div class="main-block__right">
+        <div class="txt_404">
+            <p>Сторінку не знайдено.<br>Можливо, хтось її забрав.</p>
+
+            <p>Спробуйте новий пошук,<br>або <a href="#">поверніться</a><br><a href="index.html">на головну сторінку</a>
+            </p>
+        </div>
+        <div class="image_404">
+            <img src="images/404.png" height="531" width="251" alt="">
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
+<footer>
+    <div class="main-footer">
+        <div class="main-footer__center">
+            <div class="footer-top">
+                <div class="social-block">
+                    <ul class="social-icons">
+                        <li class="icon"><a href="#" class="tweter-ico"></a></li>
+                        <li class="icon"><a href="#" class="vk-ico"></a></li>
+                        <li class="icon"><a href="#" class="facebook-ico"></a></li>
+                    </ul>
+                </div>
+                <div class="footer-form">
+                    <form>
+                        <div class="subscription-form">
+                            <input type="text" name="email" class="in-text" placeholder="Ваша електронна адреса">
+                            <input type="submit" class="btn-submit" value="Підписатись">
+                        </div>
+                        <div class="clear"></div>
+                    </form>
+                </div>
+            </div>
+            <div class="footer-center">
+                <div class="footer-logo">
+                    <a href="index.html"><img src="images/logo-footer.png" height="29" width="65" alt=""></a>
+                </div>
+                <div class="footer-menu">
+                    <ul class="nav-footer">
+                        <li class="item-nav-foot"><a href="#">Про нас</a></li>
+                        <li class="item-nav-foot"><a href="#">Переваги</a></li>
+                        <li class="item-nav-foot"><a href="#">Пошук</a></li>
+                        <li class="item-nav-foot"><a href="#">Каталог</a></li>
+                        <li class="item-nav-foot"><a href="#">Блог</a></li>
+                        <li class="item-nav-foot"><a href="#">Виробникам</a></li>
+                        <li class="item-nav-foot"><a href="#">Контакти</a></li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="footer-bottom">
+                <div class="footer-bottom__left">
+                    <p>© Наше 2015. Усі права захищені.</p>
+                </div>
+                <div class="footer-bottom__right">
+                    <p>Designed by Alex Shlafer</p>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
